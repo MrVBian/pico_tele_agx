99d22a22b4f1e6178d38ace0b64cfd95d8c4f0c4
MrVBian <544207374@qq.com>
2026-06-04 14:19:23 +0800

    feat:add shell

---
+++ 1start_tele.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source pico_tele_agx/install/setup.bash
+ros2 run ik_solver ik_solver
---
+++ 2start_can_agx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd pico_tele_agx/src/agx_arm_ros/scripts/
+bash can_muti_activate.sh
